6edeaba58fef73012ef728ed7cbe3c849bb409cb
Pawel Rogoz <sportjakopg@gmail.com>
2022-11-28 15:10:33 +0100

    Added constructors

--- src/main/java/league/types/MatchBase.java
+++ src/main/java/league/types/MatchBase.java
@@ -2,6 +2,13 @@ package league.types;
 import java.util.Date;
 
 public abstract class MatchBase {
+    public MatchBase(int firstTeamId, int secondTeamId, int match_id, Date date, String score) {
+        this.firstTeamId = firstTeamId;
+        this.secondTeamId = secondTeamId;
+        this.match_id = match_id;
+        this.date = date;
+        this.score = score;
+    }
     public int firstTeamId, secondTeamId, match_id;
 
     Date date;
--- src/main/java/league/types/TeamBase.java
+++ src/main/java/league/types/TeamBase.java
@@ -1,6 +1,10 @@
 package league.types;
 
 public class TeamBase {
+    public TeamBase(int teamID, String teamName) {
+        this.teamID = teamID;
+        this.teamName = teamName;
+    }
     public int teamID;
     public String teamName;
 }
