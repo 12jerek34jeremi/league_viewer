96596f395cfcee95a84792a18bfe32b5bfe3a807
zdamszel <01169157@pw.edu.pl>
2023-01-02 13:28:11 +0100

    update InputData.java

--- src/main/java/league/conectivity/InputData.java
+++ src/main/java/league/conectivity/InputData.java
@@ -1,15 +1,38 @@
 package league.connectivity;
-
+import java.sql.ResultSet;
 import java.sql.Connection;
 import java.util.Calendar;
 import java.util.Date;
 
 class InputData {
-    public inputPlayer(int playerid, String name, String surname, String bdate, int height, int weight, int country_id, int team_id) {
+    public inputPlayer(String name, String surname, String bdate, int height, int weight, String country_name, String team_name) {
         Connection con = BaseConector.getConnection();
         if (con == null)
             return null;
         try {
+            String get_country_id = "select country_id from countries where name like '"+ country_name + " ';";
+            PreparedStatement stmt = con.prepareStatement(get_country_id);
+            ResultSet rs = stmt.executeQuery(get_country_id);
+            int country_id = null;
+            if(rs.next())
+                country_id = rs.getInt(1);
+            
+            String get_player_id = "select max(player_id) from players;";
+            PreparedStatement stmt = con.prepareStatement(get_player_id);
+            ResultSet rs = stmt.executeQuery(get_player_id);
+            int player_id = null;
+            if(rs.next())
+                player_id = rs.getInt(1);
+            player_id = player_id + 1;
+
+            String get_team_id = "select team_id from teams where name = " + team_name + ";";
+            PreparedStatement stmt = con.prepareStatement(get_team_id);
+            ResultSet rs = stmt.executeQuery(get_team_id);
+            int team_id = null;
+            if(rs.next())
+                team_id = rs.getInt(1);
+
+
             String query = "INSERT INTO players (player_id, name, surname, birth_date, height, weight, birth_country_id, team_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement stmt=con.prepareStatement(query);
             stmt.setInt(1, playerid);
@@ -31,11 +54,26 @@ class InputData {
         }
     }
 
-    public inputTeam(String name, int team_id, String acronym, int country_id) {
+    public inputTeam(String name, String acronym, int country_id) {
         Connection con = BaseConector.getConnection();
         if(con == null)
             return null;
         try {
+            String get_team_id = "select max(team_id) from teams;";
+            PreparedStatement stmt = con.prepareStatement(get_team_id);
+            ResultSet rs = stmt.executeQuery(get_team_id);
+            int team_id = null;
+            if(rs.next())
+                team_id = rs.getInt(1);
+            team_id = team_id + 1;
+
+            String get_country_id = "select country_id from countries where name like '"+ country_name + " ';";
+            PreparedStatement stmt = con.prepareStatement(get_country_id);
+            ResultSet rs = stmt.executeQuery(get_country_id);
+            int country_id = null;
+            if(rs.next())
+                country_id = rs.getInt(1);
+
             String query = "INSERT INTO teams (name, team_id, acronym, country_id) VALUES (?, ?, ?, ?)";
             PreparedStatement stmt=con.prepareStatement(query);
             stmt.setString(1, name);
